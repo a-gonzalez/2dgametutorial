15ee8c2ee0aa9d854bef8b40e156796721b96991
Antonio Gonzalez <agonzalez@gonzaleztek.com>
2025-10-13 12:33:29 -0500

    adding main.java

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package unus.main;
+
+import javax.swing.JFrame;
+
+public class Main
+{
+    public static void main(String[] arguments) throws Exception
+    {
+        /*int rows = 21;
+        int columns = 19;
+        int tile_size = 32;
+        int width = columns * tile_size;
+        int height = rows * tile_size;*/
+
+        /*Game game = new Game();
+
+        JFrame frame = new JFrame("Pacman");
+        frame.setSize(game.getWidth(), game.getHeight());
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(game);
+        frame.pack();
+
+        game.requestFocus();
+
+        frame.setVisible(true);*/
+        System.out.println("Running...");
+    }
+}
