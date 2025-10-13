e46329c85355b2486531afefee0794c28a9502a5
Antonio Gonzalez <agonzalez@gonzaleztek.com>
2025-10-13 14:49:37 -0500

    adding run / update / draw methods, implemented Runnable

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,7 +6,9 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-public class Game extends JPanel
+//import java.lang.InterruptedException;
+
+public class Game extends JPanel implements Runnable
 {
     // screen (window) settings
     final int TILE_SIZE_ORIGINAL = 16;
@@ -18,16 +20,77 @@ public class Game extends JPanel
     final int SCREEN_HEIGHT = TILE_SIZE * SCREEN_ROWS;
 
     // world settings
-    final int MAX_WORLD_COLUMNS = 50;
-    final int MAX_WORLD_ROWS = 50;
-    final int WORLD_WIDTH = TILE_SIZE * MAX_WORLD_COLUMNS;
-    final int WORLD_HEIGHT = TILE_SIZE * MAX_WORLD_ROWS;
+    final int WORLD_COLUMNS = 50;
+    final int WORLD_ROWS = 50;
+    final int WORLD_WIDTH = TILE_SIZE * WORLD_COLUMNS;
+    final int WORLD_HEIGHT = TILE_SIZE * WORLD_ROWS;
+
+    Thread thread;
 
     public Game()
     {
         setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         setBackground(Color.BLACK);
-        setDoubleBuffered(true);
+        setDoubleBuffered(true); // better rendering performance for 2d games
         setFocusable(true);
+    }
+
+    public void setup()
+    {
+
+    }
+
+    public void start()
+    {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    int x = 10;
+    int y = 20;
+    int size = 50;
+
+    public void update()
+    {
+        x += 2;
+        y += 2;
+    }
+
+    public void draw(Graphics graphics)
+    {
+        graphics.setColor(Color.RED);
+        graphics.fillRect(x, y, size, size);
+    }
+
+    public void paintComponent(Graphics graphics)
+    {
+        super.paintComponent(graphics);
+
+        draw(graphics);
+    }
+
+    // implemenetations
+    // runnable
+    @Override
+    public void run()
+    {
+        while (thread != null)
+        {
+            //System.out.println("Runnings...");
+            // 1 UPDATE: player, npc positions / actions data
+            update();
+
+            // 2 DRAW: player, npc with UPDATE data
+            repaint();
+
+            /*try
+            {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException exception)
+            {
+                exception.printStackTrace();
+            }*/
+        }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -17,6 +17,7 @@ public class Main
         frame.pack();
 
         game.requestFocus();
+        game.start();
 
         frame.setVisible(true);
     }
